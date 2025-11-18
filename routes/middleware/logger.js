9650710232df83fd1a55cc293bc06adb907dc3fd
Shakline <saklinechy03@gmail.com>
2025-11-18 05:46:52 +0000

    Add timestamped request logger middleware

--- routes/middleware/logger.js
+++ routes/middleware/logger.js
@@ -1,8 +1,15 @@
+// routes/middleware/logger.js
+
 module.exports = function logger(req, res, next) {
   const start = Date.now();
+
   res.on('finish', () => {
     const ms = Date.now() - start;
-    console.log(`${req.method} ${req.originalUrl} -> ${res.statusCode} ${ms}ms`);
+    const timestamp = new Date().toISOString();
+    console.log(
+      `[${timestamp}] ${req.method} ${req.originalUrl} -> ${res.statusCode} ${ms}ms`
+    );
   });
+
   next();
 };
