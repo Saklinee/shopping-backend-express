90fc9592134feeface9b6c6213e2f7ff79f528bf
Shakline <saklinechy03@gmail.com>
2025-11-18 05:32:57 +0000

    Improve lessons PUT route with not-found handling

--- routes/lessons.js
+++ routes/lessons.js
@@ -4,6 +4,7 @@ const { getDB } = require('../db');
 
 const router = express.Router();
 
+// GET /lessons - return all lessons as JSON
 router.get('/', async (req, res) => {
   try {
     const lessons = await getDB().collection('lesson').find({}).toArray();
@@ -13,12 +14,20 @@ router.get('/', async (req, res) => {
   }
 });
 
+// PUT /lessons/:id - update any fields in a lesson
 router.put('/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const update = req.body;
-    const result = await getDB().collection('lesson')
+
+    const result = await getDB()
+      .collection('lesson')
       .updateOne({ _id: new ObjectId(id) }, { $set: update });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: 'Lesson not found' });
+    }
+
     res.json({ matched: result.matchedCount, modified: result.modifiedCount });
   } catch (e) {
     res.status(400).json({ error: 'Failed to update lesson' });
